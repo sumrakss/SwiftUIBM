9434045044008a1a564738a88a63cc3b021dfeb8
sumrakss <mr-noname@list.ru>
2020-09-02 23:39:14 +1100

    Initial Commit

---
+++ BrandMasterSUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BrandMasterSUI
+//
+//  Created by Алексей on 02.09.2020.
+//  Copyright © 2020 Alexey Orekhov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
